edf1a43525aefedaa1ed63af596556f201515151
Cameron Ross <elpenguino@gmail.com>
2013-09-05 23:33:17 -0300

    fix some indenting

--- binomdist.py
+++ binomdist.py
@@ -14,8 +14,8 @@ def main():
   m = user_count
   p = transmit_percent / 100
   for index, valtriple in enumerate([(binom_dist_eq  (m,k,p),
-                                 binom_dist_lteq(m,k,p),
-                                 binom_dist_gt  (m,k,p)) for k in range(0, user_limit+1)]):
+                                      binom_dist_lteq(m,k,p),
+                                      binom_dist_gt  (m,k,p)) for k in range(0, user_limit+1)]):
     print('%02d' % index, '%09.6f %09.6f %09.6f' % valtriple)
 
 def binom_dist_eq(successes, trials, probability):
