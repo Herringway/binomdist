a45d9c7642752feb673dc9f519054437661e3961
Cameron Ross <elpenguino@gmail.com>
2013-09-05 23:31:59 -0300

    change some variable names

--- binomdist.py
+++ binomdist.py
@@ -5,18 +5,18 @@ import itertools
 from scipy.stats import binom
 
 def main():
-  totallinkspeed = 1000000
-  individuallinkspeed = 100000
-  transmitpercent = 10
-  usercount = 35
-  userlimit = 10
+  total_link_speed = 1000000
+  individual_link_speed = 100000
+  transmit_percent = 10
+  user_count = 35
+  user_limit = 10
   print(' n   P(x=n)   P(x<=n)    P(x>n)')
-  m = usercount
-  p = transmitpercent / 100
-  for k,valtriple in enumerate([(binom_dist_eq  (m,k,p),
+  m = user_count
+  p = transmit_percent / 100
+  for index, valtriple in enumerate([(binom_dist_eq  (m,k,p),
                                  binom_dist_lteq(m,k,p),
-                                 binom_dist_gt  (m,k,p)) for k in range(0, userlimit+1)]):
-    print('%02d' % k, '%09.6f %09.6f %09.6f' % valtriple)
+                                 binom_dist_gt  (m,k,p)) for k in range(0, user_limit+1)]):
+    print('%02d' % index, '%09.6f %09.6f %09.6f' % valtriple)
 
 def binom_dist_eq(successes, trials, probability):
   return binom.pmf(trials, successes, probability)
