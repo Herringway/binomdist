7a8d378c04002b123812977a5d66c9564152c02a
Cameron Ross <elpenguino@gmail.com>
2013-09-05 22:33:58 -0300

    implement probability functions

--- binomdist.py
+++ binomdist.py
@@ -7,19 +7,20 @@ def main():
   totallinkspeed = 1000000
   individuallinkspeed = 100000
   transmitpercent = 10
+  usercount = 35
   userlimit = 10
   print(' n   P(x=n)   P(x<=n)    P(x>n)')
   for users in range(0,userlimit+1):
-    print('%02d %09.6f %09.6f %09.6f' % (users, binomDistEq(transmitpercent / 100, users, userlimit), binomDistLtEq(transmitpercent / 100, users, userlimit), binomDistGt(transmitpercent / 100, users, userlimit)))
+    print('%02d %09.6f %09.6f %09.6f' % (users, binomDistEq(transmitpercent / 100, users, usercount), binomDistLtEq(transmitpercent / 100, users, usercount), binomDistGt(transmitpercent / 100, users, usercount)))
 
 def binomDistEq(probability, trials, arg3):
-  return trials
+  return binom.pmf(trials, arg3, probability)
 
 def binomDistLtEq(probability, trials, arg3):
-  return trials
+  return binom.cdf(trials, arg3, probability)
 
 def binomDistGt(probability, trials, arg3):
-  return trials
+  return 1.0-binomDistLtEq(probability, trials, arg3)
 
 if __name__ == '__main__':
   main()
