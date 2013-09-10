4455c59944779c345c02da8cb373c5bdd99ef01e
Cameron Ross <elpenguino@gmail.com>
2013-09-10 16:36:39 -0300

    remove scipy dependency

--- binomdist.py
+++ binomdist.py
@@ -2,7 +2,7 @@
 
 import sys
 import itertools
-from scipy.stats import binom
+import math
 
 def main():
   total_link_speed = 1000000
@@ -18,14 +18,21 @@ def main():
                                       binom_dist_gt  (m,k,p)) for k in range(0, user_limit+1)]):
     print('%02d' % index, '%09.7f %09.7f %09.7f' % valtriple)
 
-def binom_dist_eq(successes, trials, probability):
-  return binom.pmf(trials, successes, probability)
+	
+def binom_coeff(n, k):
+  return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
 
-def binom_dist_lteq(successes, trials, probability):
-  return binom.cdf(trials, successes, probability)
+def binom_dist_eq(n, k, p):
+  return binom_coeff(n,k) * (p ** k) * ((1 - p) ** (n - k))
 
-def binom_dist_gt(successes, trials, probability):
-  return 1.0-binom_dist_lteq(successes, trials, probability)
+def binom_dist_lteq(n, k, p):
+  v = 0.0
+  for i in range(0, k+1):
+    v += binom_dist_eq(n, i, p)
+  return v
+
+def binom_dist_gt(n, k, p):
+  return 1.0-binom_dist_lteq(n, k, p)
 
 if __name__ == '__main__':
   main()
